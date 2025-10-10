70a88e8ffae229de80358a276060055d492bf321
amandarosendal <amandarosendal@hotmail.com>
2025-10-10 17:05:07 +0200

    små fix för bästa resultat

--- app.js
+++ app.js
@@ -6,3 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
         msg.textContent = 'Jag är snäll!';
     });
 });
+
+/* Så målet här är: När någon klickar på knappen "Klicka här för att se om jag är snäll" ska texten "Jag är snäll!" dyka upp.
+
+    document.addEventListener betyder att JS ska vänta tills hela HTML-sidan är klaruppbyggd innan koden under ska köras.
+
+    const btn = document.getElementById('btn');
+    Detta betyder att vi hämtar knappen från HTML.
+
+    const msg = document.getElementById('msg');
+    Detta betyder att vi hämtar textrutan från HTML.
+
+    btn.addEventListener('click', () => {
+    Detta betyder att vi har kopplat knappen till en händelse som ska ske.
+
+    msg.textContent = 'Jag är snäll!';
+    Detta är då själva texten som kommer upp när vi har tryckt på knappen.
+
+    Dessa tecken: }); betyder att det är slut på vad som händer när man har tryckt på knappen (man stänger blocket).
+*/
